38da5aaed52d9d756b233377a8c69428aa9d3bc9
Shivadumnawar <60893011+Shivadumnawar@users.noreply.github.com>
2020-03-04 17:56:45 +0530

    Add files via upload

---
+++ check_alphabet.py
@@ -0,0 +1,15 @@
+'''
+ Write a program to check whether 
+  character is an alphabet or not 
+  using conditional operator.
+'''
+
+str1="g"
+print(str1.isalpha())     # True
+
+str2= "2"
+print(str2.isalpha())     # False
+
+str3= "*"
+print(str3.isalpha())    #  False
+
