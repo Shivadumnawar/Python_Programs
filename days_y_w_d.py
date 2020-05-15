e5278bf20387c3710a483e83a769528ba2404556
Shivadumnawar <60893011+Shivadumnawar@users.noreply.github.com>
2020-05-15 17:25:21 +0530

    Update days_y_w_d.py

--- days_y_w_d.py
+++ days_y_w_d.py
@@ -1,12 +1,23 @@
-'''  Write a  program to convert days 
-        into years, weeks and days.
-'''
-days= int(input("number of days :"))
-year= days//365
-days= days%365
-weeks= days//7
-days=days%7
-num_of_days = days
-print("number of years :",year)
-print("number of weeks :",weeks)
-print("number of days :",num_of_days)
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 15 16:25:00 2020
+
+@author: shiva dumnawar
+"""
+
+'''  Write a program to convert days into years, 
+weeks and days. '''
+
+days=int(input("enter days :")) 
+
+years= days//365
+
+weeks= (days%365)//7
+
+remaining_days= (days%365)%7
+
+print("number of years :", years)
+
+print("number of weeks :", weeks)
+
+print("remaining days :", remaining_days)
