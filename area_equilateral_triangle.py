c7f7197abb74dc1aef6d784a02c325da09f353c6
Shivadumnawar <60893011+Shivadumnawar@users.noreply.github.com>
2020-05-15 16:21:48 +0530

    Update area_equilateral_triangle.py

--- area_equilateral_triangle.py
+++ area_equilateral_triangle.py
@@ -2,6 +2,6 @@
         of an equilateral triangle
 '''
 from math import sqrt
-a= 8          # a is side of a triangle
+a= 8          # a is side of a equilateral triangle
 area= sqrt(3)*(a**2)/4
 print("area of a equilateral triangle :",area)
