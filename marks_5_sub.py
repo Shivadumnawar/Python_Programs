f2dddb9079c0b7b922c1e8345f1e9f93c28b011e
Shivadumnawar <60893011+Shivadumnawar@users.noreply.github.com>
2020-05-15 16:13:03 +0530

    Update marks_5_sub.py

--- marks_5_sub.py
+++ marks_5_sub.py
@@ -2,16 +2,20 @@
      five subjets and calculate total, 
         average and percentage.
 '''
-m1=52
-m2=46
-m3=39
-m4=61
-m5=58
+m1=57  
+m2=54
+m3=49
+m4=52
+m5=45
+
+total_marks=300  # each subject consists 60 marks
 
 total_scored = m1+m2+m3+m4+m5
-total_marks=350
+
 average= total_scored/5
+
 percentage= (total_scored/ total_marks)*100
+
 print("total scored marks :",total_scored)
 print("average marks :",average)
 print("percentage :",percentage )
