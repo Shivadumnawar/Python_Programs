ce39212fa42e9778de038272b9864ecf9dfaa54e
Shivadumnawar <60893011+Shivadumnawar@users.noreply.github.com>
2020-05-16 12:19:54 +0530

    Update area_triangle.py

--- area_triangle.py
+++ area_triangle.py
@@ -2,6 +2,6 @@
 of a triangle and find its area.
 '''
 base= float(input("enter base: "))
-height= int(input("enter height: "))
+height= float(input("enter height: "))
 area = 0.5 * base * height
 print("area of a triangle :",area)
