487501086037e273730398739c57e0fe48b1dd61
Shivadumnawar <60893011+Shivadumnawar@users.noreply.github.com>
2020-05-16 11:53:29 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -5,10 +5,6 @@ Created on Sat May 16 10:45:11 2020
 @author: shiva dumnawar
 """
 
-''' GCD (Greatest Common Divisor) or 
-HCF (Highest Common Factor) of two numbers
- is the largest number that divides
- both of them.  '''
 
 # Gcd or Hcf of two numbers
 
