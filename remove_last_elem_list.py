edd3659160f2ec86da54d3761dec2898da09f3ea
Shivadumnawar <60893011+Shivadumnawar@users.noreply.github.com>
2020-05-16 23:12:52 +0530

    Create remove_last_elem_list.py

---
+++ remove_last_elem_list.py
@@ -0,0 +1,13 @@
+
+'''Create a list and remove last element
+ from it.'''
+ 
+list1=[12,28,67,158,59]
+
+print(list1)
+
+list1.remove(59)  # remove()
+
+list1.pop()  # pop removes last element
+
+del list1[-1]  # del 
